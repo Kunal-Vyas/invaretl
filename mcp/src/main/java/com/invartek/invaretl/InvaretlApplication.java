f8ded4b27ec241c925432579a84daba4cf39760c
Kunal Vyas <Kunal.Vyas@quaddimension.ca>
2026-02-10 16:34:01 -0400

    Spring AI Skeleton

---
+++ mcp/src/main/java/com/invartek/invaretl/InvaretlApplication.java
@@ -0,0 +1,13 @@
+package com.invartek.invaretl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvaretlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvaretlApplication.class, args);
+	}
+
+}
---
+++ mcp/src/test/java/com/invartek/invaretl/InvaretlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invartek.invaretl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvaretlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
