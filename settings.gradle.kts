9ebe8296c32b0f7c5c229df597bbe1d4cd9aee53
Kunal Vyas <Kunal.Vyas@quaddimension.ca>
2026-03-10 16:54:56 -0300

    Rebasing rebase

--- settings.gradle.kts
+++
@@ -1,2 +0,0 @@
-rootProject.name = "invaretl"
-include("server")
--- ui/src/views/ProjectsView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-8">
-    <h1 class="text-2xl font-bold mb-2">Projects</h1>
-    <p class="text-gray-600 dark:text-gray-400">This page is under construction.</p>
-  </div>
-</template>
